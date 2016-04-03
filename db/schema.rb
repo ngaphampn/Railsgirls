55be86b1b516051cc1597cab2e3ee0b477e879e0
ngaphampn <kayleepham0909@gmail.com>
2016-04-03 03:37:35 +0000

    type your commit message here

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402064923) do
+ActiveRecord::Schema.define(version: 20160403032559) do
 
   create_table "bingos", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20160402064923) do
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "like_count"
+    t.string   "picture"
+    t.integer  "reply_id"
   end
 
   create_table "ideas", force: :cascade do |t|
